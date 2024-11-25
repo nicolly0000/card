2139f897dc29edd6a77e56155068e1cdd52c8da8
nicolly0000 <00001106992921sp@al.educacao.sp.gov.br>
2024-11-24 23:01:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cultura Pop',
+    'Quem é o diretor de Hogwarts?',
+    'Alvo Dumbledore'
+)
+
+criaCartao(
+    'Cultura Pop',
+    'Qual a Casa de Harry Potter?',
+    'Grifinoria'
+)
+
+criaCartao(
+    'Cultura Pop',
+    'O que faz o feitiço Lumos?',
+    'Permite aos bruxos criar um ponto de luz na ponta da varinha, que pode ser usado como lanterna'
+)
+
+criaCartao(
+    'Cultura Pop',
+    'Qual o nome dos Pais de Harry Potter?',
+    'James/Tiago Potter e Lílian Evans Potter'
+)
